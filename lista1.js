4cce94bbc1ea5b14aabaa109c5014234e5a69420
SinousSeb <52143725+SinousSeb@users.noreply.github.com>
2023-06-30 18:44:18 -0500

    Add files via upload

---
+++ lista1.js
@@ -0,0 +1,12 @@
+const lista=[
+    "Sebastian Monsalve Palacio",
+    18,
+    true,
+    new Date (2004,10,15),
+    {
+        titulo: "Memorias de un hijueputa",
+        autor: "Fernando Vallejo",
+        fecha: new Date ("june 1994"),
+        URL: "https://www.amazon.com/-/es/Fernando-Vallejo-ebook/dp/B07QLGSSKD/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=PXRCPZAL605M&keywords=memorias+de+un+hijueputa&qid=1688168292&sprefix=memorias+de+un+hijueputa%2Caps%2C172&sr=8-1"
+    },
+];
